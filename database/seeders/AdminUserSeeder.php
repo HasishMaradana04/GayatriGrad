9583a56f022e98495d5f9bfb22442f2a5590585e
Hasish Maradana <hasish042007@gmail.com>
2026-06-07 19:09:33 +0530

    Feat: Update Super Admin credentials in seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -11,11 +11,11 @@ class AdminUserSeeder extends Seeder
     public function run(): void
     {
         $users = [
-            ['name' => 'Super Admin', 'email' => 'superadmin@example.com', 'role' => 'Super Admin'],
-            ['name' => 'Events Manager', 'email' => 'events@example.com', 'role' => 'Events Manager'],
-            ['name' => 'Gallery Manager', 'email' => 'gallery@example.com', 'role' => 'Gallery Manager'],
-            ['name' => 'News Manager', 'email' => 'news@example.com', 'role' => 'News Manager'],
-            ['name' => 'Faculty Manager', 'email' => 'faculty@example.com', 'role' => 'Faculty Manager'],
+            ['name' => 'Super Admin', 'email' => 'hasishmaradana@gmail.com', 'role' => 'Super Admin', 'password' => 'hasish@2007'],
+            ['name' => 'Events Manager', 'email' => 'events@example.com', 'role' => 'Events Manager', 'password' => 'Password@123'],
+            ['name' => 'Gallery Manager', 'email' => 'gallery@example.com', 'role' => 'Gallery Manager', 'password' => 'Password@123'],
+            ['name' => 'News Manager', 'email' => 'news@example.com', 'role' => 'News Manager', 'password' => 'Password@123'],
+            ['name' => 'Faculty Manager', 'email' => 'faculty@example.com', 'role' => 'Faculty Manager', 'password' => 'Password@123'],
         ];
 
         foreach ($users as $admin) {
@@ -23,7 +23,7 @@ class AdminUserSeeder extends Seeder
                 ['email' => $admin['email']],
                 [
                     'name' => $admin['name'],
-                    'password' => Hash::make('Password@123'),
+                    'password' => Hash::make($admin['password']),
                     'is_active' => true,
                 ],
             );
